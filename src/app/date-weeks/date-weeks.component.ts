371d20365976d85fba58e0020e6253437d5ad9aa
yersoncp <yerson.rc@gmail.com>
2019-06-13 14:59:28 -0500

    1 file modified

--- src/app/date-weeks/date-weeks.component.ts
+++ src/app/date-weeks/date-weeks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import moment from 'moment';
-import {Moment} from 'moment';
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-date-weeks',
